c8c89b81f9f2ff90dd2d055cbb8e34bcd26a6856
Mounika Sharan <mounikar523@gmail.com>
2022-10-22 09:54:09 +0530

    updated job field

--- src/app/features/jobs/jobs.component.ts
+++ src/app/features/jobs/jobs.component.ts
@@ -1,15 +1,57 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
 
+import { HttpCommonService } from 'src/app/core/services/httpCommon.service';
+
+
+
 @Component({
+
   selector: 'app-jobs',
+
   templateUrl: './jobs.component.html',
+
   styleUrls: ['./jobs.component.scss']
+
 })
+
 export class JobsComponent implements OnInit {
 
-  constructor() { }
+
+
+  jobs:any[] = [];
+
+  constructor(public httpClient: HttpCommonService) { }
+
+
 
   ngOnInit(): void {
+
+    this.getAllJobs();
+
   }
+
+
+
+  getAllJobs(): void {
+
+    this.httpClient.get('Openjobs/GetOpenjobs').subscribe((data:any) => {
+
+      this.jobs = data;
+
+      },
+
+      (error: HttpErrorResponse) => {
+
+      console.log (error.name + ' ' + error.message);
+
+      });
+
+
+
+  }
+
+
 
 }
