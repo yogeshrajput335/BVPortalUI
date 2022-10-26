f3ad3b47965031f85658faecdd74a870747d0538
Yogesh Rajput <Yogesh.Rajput@meritarc.com>
2022-10-26 10:58:58 +0530

    update profile page

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { HttpCommonService } from 'src/app/core/services/httpCommon.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  basicInfo:any
+  constructor(public httpClient: HttpCommonService, private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.getEmployeeBasicInfo();
+  }
+
+  getEmployeeBasicInfo(): void {
+    this.httpClient.get('EmployeeBasicInfo/GetEmployeeBasicInfoByEmpId/'+this.authenticationService.getUser().employeeId).subscribe((data:any) => {
+      this.basicInfo = data;
+      },
+      (error: HttpErrorResponse) => {
+      console.log (error.name + ' ' + error.message);
+      });
+
   }
 
 }
